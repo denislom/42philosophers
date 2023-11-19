f00a423798f6ba42096a05ccadd4a05a1316a694
Denis Lom <lom.denis@seznam.cz>
2023-11-20 00:28:03 +0100

    folder philo

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 22:47:25 by dlom              #+#    #+#             */
-/*   Updated: 2023/11/20 00:04:53 by dlom             ###   ########.fr       */
+/*   Updated: 2023/11/20 00:17:06 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 /* 
 SOURCES:
+https://suspectedoceano.notion.site/Philosophers-b1bf3c57eee6420cafa7d0900b3d3216
 Oceano:
 https://www.youtube.com/watch?v=zOpzGHwJ3MU&t=798s
 https://medium.com/@jalal92/the-dining-philosophers-7157cc05315 
@@ -26,13 +27,21 @@ https://www.youtube.com/playlist?list=PLfqABt5AS4FmuQf70psXrsMLEDQXNkLq2
 void *routine()
 {
 	ft_printf("Test from threads\n");
+	sleep(3);
+	ft_printf("Ending thread.\n");	
 }
 
 int	main(int argc, char **argv)
 {
 	pthread_t	t1;
+	pthread_t	t2;
 
-	pthread_create(&t1, NULL, &routine, NULL);
+
+	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
+		return (1);
+	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
+		return (1);
 	pthread_join(t1, NULL); // to wait for the thread, as wait for process
+	pthread_join(t2, NULL);
 	return (0);
 }
