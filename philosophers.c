644adb26af31e8d49a9a65efb53052b9fb499335
Denis Lom <lom.denis@seznam.cz>
2023-11-19 22:51:45 +0100

    creating files

---
+++ philosophers.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/19 22:47:25 by dlom              #+#    #+#             */
+/*   Updated: 2023/11/19 22:49:10 by dlom             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+/* 
+SOURCES:
+https://www.youtube.com/watch?v=zOpzGHwJ3MU&t=798s
+https://medium.com/@jalal92/the-dining-philosophers-7157cc05315 
+https://www.codequoi.com/en/threads-mutexes-and-concurrent-programming-in-c/
+*/
