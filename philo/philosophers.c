cabeeccb26c94b22be6b0ebcfad6425822956f56
Denis Lom <lom.denis@seznam.cz>
2023-11-22 01:03:08 +0100

    header file prepared, starting with main

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 22:47:25 by dlom              #+#    #+#             */
-/*   Updated: 2023/11/20 00:44:39 by dlom             ###   ########.fr       */
+/*   Updated: 2023/11/22 01:02:34 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,28 +22,19 @@ https://www.codequoi.com/en/threads-mutexes-and-concurrent-programming-in-c/
 Threads CodeVault:
 https://www.youtube.com/playlist?list=PLfqABt5AS4FmuQf70psXrsMLEDQXNkLq2
 
+./philo 5 800 200 200 [5]
+./philo {number_of_philosophers} {time_to_die} {time_to_eat} {time_to_sleep} 
+		[number_of_times_each_philosopher_must_eat]
 */
-
-void	*routine()
-{
-	ft_printf("Test from threads\n");
-	sleep(3);
-	ft_printf("Ending thread.\n");
-}
 
 int	main(int argc, char **argv)
 {
-	pthread_t	t1;
-	pthread_t	t2;
+	if (5 == argc || 6 == argc)
+	{
 
-	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
-		return (1);
-	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
-		return (2);
-	if (pthread_join(t1, NULL) != 0)
-		return (3);
-	if (pthread_join(t2, NULL) != 0)
-		return (4);
+	}
+	else
+		return(1);
 	return (0);
 }
 
