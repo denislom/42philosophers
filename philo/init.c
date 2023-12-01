43c18aa7e4f6ab183685ff12c93105e0fc496283
Denis Lom <lom.denis@seznam.cz>
2023-12-01 01:17:00 +0100

    4742

--- philo/init.c
+++ philo/init.c
@@ -6,14 +6,58 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 22:55:12 by dlom              #+#    #+#             */
-/*   Updated: 2023/11/27 22:23:46 by dlom             ###   ########.fr       */
+/*   Updated: 2023/12/01 00:20:26 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+/* 
+typedef struct s_table	t_table;
+
+struct s_table
+{
+	long	nbr_philo;
+	long	time_to_die;
+	long	time_to_eat;
+	long	time_to_sleep;
+	long	meals_limit;
+	long	simulation_start;
+	bool	simulation_end;
+	t_fork	*forks;
+	t_philo	*philos;
+};
+
+typedef struct s_philo
+{
+	int			id;
+	long		meals_eaten;
+	bool		estoy_lleno;
+	long		last_meal_time;
+	t_fork		*left_fork;
+	t_fork		*right_fork;
+	pthread_t	thread_id;
+	t_table		*table;
+}	t_philo;
+
+typedef struct s_fork
+{
+	t_mtx	fork;
+	int		fork_id;
+}	t_fork;
+*/
+
 void	data_init(t_table *table)
 {
+	int	i;
+
+	i = 0;
 	table->simulation_end = false;
-	table->philos = safe_malloc();
+	table->philos = safe_malloc(sizeof(t_philo) * table->nbr_philo);
+	table->forks = safe_malloc(sizeof(t_fork) * table->nbr_philo);
+	while (++i < table->nbr_philo)
+	{
+		safe_mutex(&table->forks[i].fork, INIT);
+	}
+	
 }
