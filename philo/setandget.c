d8f1ef2412e8106ef70e38e5698e54150630cf8c
Denis Lom <lom.denis@seznam.cz>
2024-01-06 00:19:39 +0100

    LOCK and UNLOCK repetition

---
+++ philo/setandget.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   setandget.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/06 00:14:52 by dlom              #+#    #+#             */
+/*   Updated: 2024/01/06 00:19:03 by dlom             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+void	set_bool(t_mtx	*mutex, bool *dest, bool value)
+{
+	safe_mutex(mutex, LOCK);
+	*dest = value;
+	safe_mutex(mutex, UNLOCK);
+}
+
+bool	get_bool(t_mtx *mutex, bool *value)
+{
+	bool	ret;
+
+	safe_mutex(mutex, LOCK);
+	ret = *value;
+	safe_mutex(mutex, UNLOCK);
+	return (ret);
+}
+
+long	get_long(t_mtx *mutex, long *value)
+{
+	long	ret;
+
+	safe_mutex(mutex, LOCK);
+	ret = *value;
+	safe_mutex(mutex, UNLOCK);
+	return (ret);
+}
+
+void	set_long(t_mtx *mutex, long *dest, long value)
+{
+	safe_mutex(mutex, LOCK);
+	*dest = value;
+	safe_mutex(mutex, UNLOCK);
+}
+
+bool	simulation_finished(t_table *table)
+{
+	return (get_bool(&table->table_mutex, &table->simulation_end));
+}
