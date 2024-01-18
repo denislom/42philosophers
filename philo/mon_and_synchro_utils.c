34b3424d9237684432e127d6c683c134d3dc98ac
Denis Lom <lom.denis@seznam.cz>
2024-01-19 00:37:42 +0100

    wait_threads

--- philo/mon_and_synchro_utils.c
+++ philo/mon_and_synchro_utils.c
@@ -6,9 +6,14 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 15:47:32 by dlom              #+#    #+#             */
-/*   Updated: 2024/01/18 15:41:29 by dlom             ###   ########.fr       */
+/*   Updated: 2024/01/19 00:37:25 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+void	wait_threads(t_table *table)
+{
+		while (!get_bool(&table->table_mutex, &table->threads_are_ready))
+		;
+}
