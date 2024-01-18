c0513f26838c51214295beb27c5152e51cfcff3b
Denis Lom <lom.denis@seznam.cz>
2024-01-18 17:47:09 +0100

    remake 28/82

--- philo/error_handling.c
+++ philo/error_handling.c
@@ -6,9 +6,14 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 01:04:19 by dlom              #+#    #+#             */
-/*   Updated: 2024/01/18 15:41:07 by dlom             ###   ########.fr       */
+/*   Updated: 2024/01/18 17:30:34 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+void	print_error(const char *error)
+{
+	printf("%s\n", error);
+	exit(EXIT_FAILURE);
+}
